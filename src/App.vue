40755e88acea1dfb25af062ef89f1f84877a63c3
startang141 <bintangadiyatma141@gmail.com>
2024-10-28 16:50:52 +0700

    use options and composition

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import MainButton from './components/MainButton.vue'
+import OptionsComponent from './components/OptionsComponent.vue'
+import CompositionComponent from './components/CompositionComponent.vue'
 </script>
 
 <template>
   <MainButton />
+  <OptionsComponent />
+  <CompositionComponent />
   <h1>Halooo</h1>
 </template>
 
