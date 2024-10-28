ba3c19e8df52669ac5f78f0c64513af52ecd619e
startang141 <bintangadiyatma141@gmail.com>
2024-10-28 16:50:21 +0700

    Options API

---
+++ src/components/OptionsComponent.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'favian Options',
+    }
+  },
+  methods: {
+    showName() {
+      console.log(this.name)
+    },
+  },
+}
+</script>
+<template>
+  <h2>{{ name }}</h2>
+  <button @click="showName">Tampilkan Nama</button>
+</template>
+
+<style scoped></style>
