3af52f7be048968632a5e5ebf1e7cf2eea312339
startang141 <bintangadiyatma141@gmail.com>
2024-10-28 16:50:37 +0700

    Composition API

---
+++ src/components/CompositionComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+const name = 'favian composition'
+
+const showName = () => {
+  console.log(name)
+}
+</script>
+
+<template>
+  <h2>{{ name }}</h2>
+  <button @click="showName">Tampilkan Nama</button>
+</template>
+
+<style scoped></style>
